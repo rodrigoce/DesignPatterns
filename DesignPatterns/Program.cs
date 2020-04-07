47f62a76f19247e2ae2f7f19b263db9032d21684
Rodrigo <rodrigo.ce@gmail.com>
2020-04-06 21:36:59 -0300

    Adicionar arquivos de projeto.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha o Design Pattern");
+            Console.WriteLine("");
+            Console.WriteLine("a - Fluent Builder Inheritance With Recursive Generics");
+            var c = Console.ReadKey();
+
+            switch (c.KeyChar)
+            {
+                case 'a':
+                    FluentBuilderInheritanceWithRecursiveGenerics.Program2.Main2();
+                    break; 
+                default:
+                    break;
+            }
+        }
+    }    
+}
